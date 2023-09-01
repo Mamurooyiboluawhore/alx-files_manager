ef732871c6ca4840b89f0d5c4b10124d38fb3d26
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-09-01 23:28:55 +0000

    utils

---
+++ utils/redis.js
@@ -0,0 +1,67 @@
+const redis = require('redis');
+
+
+class RedisClient {
+  constructor(host, port) {
+    this.client = redis.createClient ({
+      host: host || 'localhost',
+    port: port || 6379,
+    });
+
+  this.client.on('error', (err) => {
+    console.error('Redis Error:', err)
+  });
+  
+}
+
+  async isAlive() {
+    return new Promise(( resolve) => {
+      this.client.ping((err, reply) => {
+         if (err) {
+           resolve(false);
+    } else {
+    resolve(reply === 'PONG');
+  }
+});
+});
+}
+
+  async get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply);
+      }
+    });
+  });
+}
+
+  async set (key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(true);
+      }
+    });
+  });
+}
+
+  async del (key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(reply === 1);
+       }
+    });
+  });
+}
+
+const redisClient = new RedisClient();
+
+module.export = redisClient;
